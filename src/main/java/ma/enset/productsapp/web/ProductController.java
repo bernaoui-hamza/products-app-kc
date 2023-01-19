39ede5ae69c4b6abf3c93a4610488621892a023a
bernaoui-hamza <BERNAOUI_HAMZA@emsi-edu.ma>
2023-01-19 23:22:15 +0100

    commit 19/01/23

--- src/main/java/ma/enset/productsapp/web/ProductController.java
+++ src/main/java/ma/enset/productsapp/web/ProductController.java
@@ -1,18 +1,32 @@
 package ma.enset.productsapp.web;
 
 import ma.enset.productsapp.repositories.ProductRepository;
+import org.keycloak.KeycloakPrincipal;
+import org.keycloak.KeycloakSecurityContext;
+import org.keycloak.adapters.springsecurity.client.KeycloakRestTemplate;
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.hateoas.PagedModel;
 
 @Controller
 public class ProductController{
     @Autowired
     private ProductRepository productRepository;
-
+    @Autowired
+private KeycloakRestTemplate keycloakRestTemplate;
     @GetMapping("/")
     public String index(){
+
+     //   System.out.println("salam");
         return "index";
     }
     @GetMapping("/products")
@@ -21,8 +35,31 @@ public class ProductController{
         return "products";
     }
     @GetMapping("/suppliers")
-    public String suppliers(){
+    public String suppliers(Model model){
+        PagedModel<Supplier> pageSupplier = keycloakRestTemplate.getForObject("http://localhost:8083/suppliers", PagedModel.class);
+       model.addAttribute("suppliers",pageSupplier);
         return "suppliers";
     }
 
+    @GetMapping("/jwt")
+    @ResponseBody
+    public Map<String,String> map (HttpServletRequest request){
+        KeycloakAuthenticationToken token= (KeycloakAuthenticationToken) request.getUserPrincipal();
+        KeycloakPrincipal principal= (KeycloakPrincipal) token.getPrincipal();
+        KeycloakSecurityContext context=principal.getKeycloakSecurityContext();
+        Map<String,String>mp=new HashMap<>();
+        mp.put("AccessToken",context.getTokenString());
+return mp;
+    }
+    @ExceptionHandler(Exception.class)
+    public String ExcemptionHandler(Exception e,Model model){
+        model.addAttribute("errorMessage",e.getMessage());
+        return "errors";
+    }
+
+}
+class Supplier{
+    private Long id;
+    private String name;
+    private String email;
 }
